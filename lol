d1a554dfeeb92c167f66e3bf00985fa21f4dedb3
vladimir-bayezid <47465042+vladimir-bayezid@users.noreply.github.com>
2019-02-10 22:41:52 +0700

    Add files via upload

---
+++ lol
@@ -0,0 +1,11 @@
+#############################
+#                 credit by : Mr.Vladimir                 #
+#                 WA   : +6281930497165                 #
+#                   Minggu, 10 feb 2019                    #
+#   Recode gak bakal bikin lu jadi master   #
+#############################
+
+
+z="
+";mz='PROG';Kz='expo';qz='m☆\[';Wz='34;1';Hz='ME" ';Sz='033[';Ez='t " ';cz='t\[\';Az='figl';rz=';1m•';Nz='\033';Zz='[\03';Lz='rt P';Rz='|\[\';Dz='slan';hz='3[31';kz='33[3';gz='1m★\';uz='32;3';Bz='et -';Yz='__|\';Fz='  WE';fz='1m|';az='3[33';Tz='33;1';tz='•\[\';Jz='lcat';vz='3;1m';Pz='1m__';Qz='____';sz='2;1m';dz='__';lz='1;1m';bz=';1m\';Oz='[34;';Cz='f sm';Gz='LLCO';Vz='r\[\';pz='31;1';Uz='muse';wz='>>'\''';nz='RAME';oz='R\[\';Xz='m|__';Iz='| lo';jz='\[\0';iz=';1m☆';ez='[35;';Mz='S1='\''';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$z$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Sz$Wz$Xz$Qz$Yz$Zz$az$bz$cz$Sz$Wz$Xz$Qz$Qz$Qz$Qz$Qz$Qz$Qz$dz$z$Nz$ez$fz$z$Nz$ez$fz$z$Nz$ez$gz$Zz$hz$iz$jz$kz$lz$mz$nz$oz$Sz$pz$qz$Nz$ez$gz$Zz$hz$rz$jz$kz$sz$tz$Sz$uz$vz$wz"
---
+++ setup.sh
@@ -0,0 +1,11 @@
+#############################
+#                 credit by : Mr.Vladimir                 #
+#                 WA   : +6281930497165                 #
+#                   Minggu, 10 feb 2019                    #
+#   Recode gak bakal bikin lu jadi master   #
+#############################
+
+
+z="
+";Zz='[33;';uz='}PER';OBz='au}[';Wz='37;1';Kz='le='\''';UBz='u}]"';KBz='gem ';QBz='yan}';XBz='lal.';WBz='ahdi';Cz='33[3';RBz='STAR';Uz='m'\''';Oz='cyan';DBz='grad';lz='g}ST';EBz='e';oz='ah}]';Bz='='\''\0';rz='p 2';Hz='3[32';Rz='h='\''\';ez='r';sz=' ${h';MBz='at';Jz='purp';xz='apt ';bz='gk='\''';TBz='hija';Iz=';1m'\''';NBz='{hij';Ez=''\''';Tz='31;1';Nz='1m'\''';CBz='t up';Lz='\033';Pz='6;1m';Gz=''\''\03';iz='ah}[';IBz='et';ABz='te &';BBz='& ap';LBz='lolc';tz='ijau';kz='unin';cz='7m'\''';nz='NG $';fz='echo';az='txtb';gz=' "…$';hz='{mer';mz='ARTI';vz='SIAP';yz='upda';Qz='mera';PBz=' ${c';Az='biru';pz='"';VBz='sh l';Vz='puti';Xz='kuni';SBz='T ${';Dz='4;1m';dz='clea';qz='slee';HBz='figl';Fz='ijo=';Mz='[35;';GBz='all ';jz=' ${k';JBz='ruby';Yz='ng='\''';wz='AN $';Sz='033[';FBz='inst';YBz='sh';
+eval "$Az$Bz$Cz$Dz$Ez$z$Fz$Gz$Hz$Iz$z$Jz$Kz$Lz$Mz$Nz$z$Oz$Bz$Cz$Pz$Ez$z$Qz$Rz$Sz$Tz$Uz$z$Vz$Rz$Sz$Wz$Uz$z$Xz$Yz$Lz$Zz$Nz$z$az$bz$Lz$Zz$cz$z$dz$ez$z$fz$gz$hz$iz$jz$kz$lz$mz$nz$hz$oz$pz$z$qz$rz$z$fz$gz$hz$iz$sz$tz$uz$vz$wz$hz$oz$pz$z$qz$rz$z$xz$yz$ABz$BBz$CBz$DBz$EBz$z$xz$FBz$GBz$HBz$IBz$z$xz$FBz$GBz$JBz$z$KBz$FBz$GBz$LBz$MBz$z$dz$ez$z$fz$gz$hz$iz$jz$kz$lz$mz$nz$hz$oz$pz$z$fz$gz$hz$iz$sz$tz$uz$vz$wz$hz$oz$pz$z$fz$gz$NBz$OBz$PBz$QBz$RBz$SBz$TBz$UBz$z$qz$rz$z$dz$ez$z$VBz$WBz$XBz$YBz"
